2ffd3e0f000c29827915e52a92a418308133bb95
Garv <garv1908@gmail.com>
2025-02-01 22:13:19 -0500

    add movement functions and refactor

--- sketch_feb1a/sketch_feb1a.ino
+++ sketch_feb1a/sketch_feb1a.ino
@@ -6,25 +6,37 @@
 #define S3 5
 #define sensorOut 6
 
+
+// Motor A connections
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+// Motor B connections
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+int speed = 255;
+
 Servo servo;
 
 const int servoPin = 7;
 int pos = 0;
 
 // Variables for Color Pulse Width Measurements
-int redPW = 0;
-int greenPW = 0;
-int bluePW = 0;
+int r = 0;
+int g = 0;
+int b = 0;
 
 void colourSetup() {
-	// colour stuff
-	// Set S0 - S3 as outputs
-	pinMode(OE_PIN, OUTPUT);
-	digitalWrite(OE_PIN, LOW);
-	pinMode(S0, OUTPUT);
-	pinMode(S1, OUTPUT);
-	pinMode(S2, OUTPUT);
-	pinMode(S3, OUTPUT);
+  // colour stuff
+  // Set S0 - S3 as outputs
+  pinMode(OE_PIN, OUTPUT);
+  digitalWrite(OE_PIN, LOW);
+  pinMode(S0, OUTPUT);
+  pinMode(S1, OUTPUT);
+  pinMode(S2, OUTPUT);
+  pinMode(S3, OUTPUT);
 
 	// Set Pulse Width scaling to 20%
 	digitalWrite(S0,HIGH);
@@ -34,10 +46,26 @@ void colourSetup() {
 	pinMode(sensorOut, INPUT);
 }
 
+void motorSetup() {
+  // Set all the motor control pins to outputs
+	pinMode(enA, OUTPUT);
+	pinMode(enB, OUTPUT);
+	pinMode(in1, OUTPUT);
+	pinMode(in2, OUTPUT);
+	pinMode(in3, OUTPUT);
+	pinMode(in4, OUTPUT);
+	
+	// Turn off motors - Initial state
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, LOW);
+}
+
 void setup() {
 	// Setup Serial Monitor
 	Serial.begin(9600);
-
+  motorSetup();
 	colourSetup();
 
 	// servo writup
@@ -48,22 +76,71 @@ void setup() {
 
 void loop() {
 	// GET PULSE WIDTH
-	redPW = getRedPW();
+	r = getRedPW();
 	// Delay to stabilize sensor
 	delay(200);
 
-	greenPW = getGreenPW();
+	g = getGreenPW();
 	delay(200);
 
-	bluePW = getBluePW();
+	b = getBluePW();
 	delay(200);
 
+  setMotorSpeed();
+  goStraight();
+  delay(2000);
+  while (!isBlack(r, g, b)) {
+    goStraight();
+  }
 	colourTest();
+
 
 }
 
+bool isBlack(int r, int g, int b) {
+  return (r > 200 && g > 200 && b > 200);
+}
+bool isRed(int r, int g, int b) {
+  return (r < g && r < b);
+}
+bool isGreen(int r, int g, int b) {
+  return (g < r && g < b);
+}
+bool isBlue(int r, int g, int b) {
+  return (b < g && b < r);
+}
+
+void setMotorSpeed(int speed) {
+  // Set motors to maximum speed
+	// For PWM maximum possible values are 0 to 255
+	analogWrite(enA, speed);
+	analogWrite(enB, speed);
+}
+
 void goStraight() {
-  
+  // Turn on motor A & B
+	digitalWrite(in1, HIGH);
+	digitalWrite(in2, LOW);
+	digitalWrite(in3, HIGH);
+	digitalWrite(in4, LOW);
+	delay(2000);
+}
+
+void reverse() {
+  	// Turn on motor A & B
+	digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, LOW);
+	digitalWrite(in4, HIGH);
+	delay(2000);
+}
+
+void uTurn() {
+  digitalWrite(in1, LOW);
+	digitalWrite(in2, HIGH);
+	digitalWrite(in3, HIGH);
+	digitalWrite(in4, LOW);
+
 }
 
 
@@ -71,11 +148,11 @@ void goStraight() {
 void colourTest() {
 	// Print output to Serial Monitor
 	Serial.print("Red PW = ");
-	Serial.print(redPW);
+	Serial.print(r);
 	Serial.print(" - Green PW = ");
-	Serial.print(greenPW);
+	Serial.print(g);
 	Serial.print(" - Blue PW = ");
-	Serial.println(bluePW);
+	Serial.println(b);
 }
 
 void clawTest() {
